bf74671431af8217db046a9ff00b231ea2e1cc98
Oliver Anderson <oander@uwo.ca>
2024-11-23 12:47:41 -0500

    testing

---
+++ script.js
@@ -0,0 +1,16 @@
+import { HfInference } from '@huggingface/inference'
+
+const hf = new HfInference('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')
+
+let testies = async function(prompt)
+{
+    let out = await hf.textGeneration({
+        model: 'TinyLlama/TinyLlama-1.1B-Chat-v1.0',
+        inputs: prompt
+      })
+    return(out.generated_text);
+}
+
+let theThing = document.getElementById("thing");
+theThing.innerText = "calculating";
+theThing.innerText = await testies("my favourite horse is");
